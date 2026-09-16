d41d560d8cd393122f124f2dfcfa464de2be0bea
kielbasan1 <kielbasan1@users.noreply.github.com>
2026-09-16 17:09:30 +0300

    sw.js: scope dışı ikonları cache listesinden çıkar

--- dilekce/sw.js
+++ dilekce/sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'dilekce-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', '../icons/icon-192.png', '../icons/icon-512.png'];
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
